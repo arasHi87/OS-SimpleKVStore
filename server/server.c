9e1fbd7b22460adf4affcac7e3e7e6a8799471e1
arashi87 <arasi27676271@gmail.com>
2021-11-24 18:01:52 +0800

    Init: refactor arch, and lint style
    Because ths plan is to rewrite the socket, so remove the socket default
    code by TA, and also lint the coding style

--- server/server.c
+++ server/server.c
@@ -1,43 +1,37 @@
+#include <ctype.h>
+#include <netdb.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <ctype.h>
 #include <string.h>
-#include <netdb.h>
-#include <ctype.h>
+#include <unistd.h>
 
 #include "types.h"
-#include "sock.h"
 
 int main(int argc, char **argv)
 {
     char *server_port = 0;
     int opt = 0;
     /* Parsing args */
-    while ((opt = getopt(argc, argv, "p:")) != -1) {
-        switch (opt) {
+    while ((opt = getopt(argc, argv, "p:")) != -1)
+    {
+        switch (opt)
+        {
         case 'p':
             server_port = malloc(strlen(optarg) + 1);
             strncpy(server_port, optarg, strlen(optarg));
             break;
         case '?':
-            fprintf(stderr, "Unknown option \"-%c\"\n", isprint(optopt) ?
-                    optopt : '#');
+            fprintf(stderr, "Unknown option \"-%c\"\n", isprint(optopt) ? optopt : '#');
             return 0;
         }
     }
 
-    if (!server_port) {
+    if (!server_port)
+    {
         fprintf(stderr, "Error! No port number provided!\n");
         exit(1);
     }
 
-    /* Open a listen socket fd */
-    int listenfd __attribute__((unused)) = open_listenfd(server_port);
-
-    /* Start coding your server code here! */
-
     return 0;
 }
-
